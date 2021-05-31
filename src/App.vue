7cfafc1c5d1028dbbe02ba851f34d59e383cadd9
Asher <asyncola@gmail.com>
2021-05-31 14:58:55 +0800

    引入postcss封装request请求工具

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <!-- 路由出口 -->
-    <router-view/>
+    <router-view />
     <h1>明日头条</h1>
+    <i class="toutiao toutiao-zuopin"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+    <div class="box"></div>
   </div>
 </template>
 
@@ -13,4 +24,9 @@ export default {
 </script>
 
 <style lang="less">
+.box {
+  width: 375px;
+  height: 60px;
+  background-color: #f90;
+}
 </style>
